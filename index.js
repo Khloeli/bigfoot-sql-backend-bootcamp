e54beb14cf5c4c4254b454acc3d170145a6662b0
khloeli <chloelihuimin@gmail.com>
2023-07-29 08:34:34 +0800

    done base

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
 
+// enable cors for all routes
+app.use(cors());
+
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+// set sightingIndex as the params
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  res.json(sightings[req.params.sightingIndex]);
 });
 
 app.listen(PORT, () => {
