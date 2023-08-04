2438b7d0f5a0db84d684b7e73befd1d14aca5890
khloeli <chloelihuimin@gmail.com>
2023-08-05 06:39:57 +0800

    did bigfoot-sql more comfortable

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -5,11 +5,51 @@ class SightingsController extends BaseController {
     super(model);
   }
 
+  //findByPk is used to search for a single instance by its primary key. PK is short for Primary Key, a unique identifier for each record in a database table. In your code, sightingId is used as the primary key to find a specific sighting.
   // Retrieve specific sighting
   async getOne(req, res) {
     const { sightingId } = req.params;
     try {
       const sighting = await this.model.findByPk(sightingId);
+      return res.json(sighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  // Create sightings
+  async createSighting(req, res) {
+    const { date, location, notes } = req.body;
+    try {
+      const sighting = await this.model.create({
+        date: date,
+        location: location,
+        notes: notes,
+      });
+      console.log(this.model.location);
+      return res.json(sighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  // edit Sightings
+  async editSighting(req, res) {
+    const { sightingId } = req.params;
+    const { date, location, notes } = req.body;
+    try {
+      const sighting = await this.model.update(
+        {
+          date: date,
+          location: location,
+          notes: notes,
+        },
+        {
+          where: {
+            id: sightingId,
+          },
+        }
+      );
       return res.json(sighting);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const app = express();
 
 // Enable CORS access to this server
 app.use(cors());
+app.use(express.json());
 
 // using the routers
 app.use("/sightings", sightingRouter);
@@ -32,29 +33,3 @@ app.listen(PORT, () => {
 
   console.log("testing");
 });
-
-// const express = require("express");
-// const cors = require("cors");
-// require("dotenv").config();
-// const { getSightings } = require("./utils.js");
-
-// const PORT = process.env.PORT;
-// const app = express();
-
-// // enable cors for all routes
-// app.use(cors());
-
-// app.get("/sightings", async (req, res) => {
-//   const sightings = await getSightings();
-//   res.json(sightings);
-// });
-
-// // set sightingIndex as the params
-// app.get("/sightings/:sightingIndex", async (req, res) => {
-//   const sightings = await getSightings();
-//   res.json(sightings[req.params.sightingIndex]);
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Express app listening on port ${PORT}!`);
-// });
